7b5ff0f836c0b26085b8fc272bb1f3030aab0879
Jordi Porcar Muñoz <jorpormun@alu.edu.gva.es>
2024-11-15 10:20:41 +0100

    Ejercios cadenas

--- src/main/SumaBinaria.java
+++ src/main/SumaBinaria.java
@@ -1,36 +1,34 @@
 public class SumaBinaria {
 
-    public static String sumarBinarios(String bin1, String bin2) {
+    public static String sumarBinarios(String num1, String num2) {
 
         String resultado = "";
-        int acarreo = 0;
+        int digitoA = Integer.parseInt("" + num1.charAt(0));
+        char b1;
+        char b2;
+        int bit1;
+        int bit2;
+        int acarreo = 0 ;
+        int suma;
+        for(int i = num1.length() - 1; i >= 0; i--) {
 
-        if (bin1.length() < bin2.length()) {
-            for (int i = bin1.length(); i < bin2.length(); i++) {
-                bin1 = "0" + bin1;
-            }
-        } else if (bin2.length() < bin1.length()) {
-            for (int i = bin2.length(); i < bin1.length(); i++) {
-                bin2 = "0" + bin2;
-            }
-        }
-        for (int i = bin1.length() - 1; i >= 0; i--) {
-            int bit1 = bin1.charAt(i) - '0';
-            int bit2 = bin2.charAt(i) - '0';
+            b1 = num1.charAt(i);
+            b2 = num2.charAt(i);
+            bit1 = Integer.parseInt("" + b1);
+            bit2 = Integer.parseInt("" + b2);
+            suma = bit1 + bit2 + acarreo;
 
-            int suma = bit1 + bit2 + acarreo;
-            resultado = (suma % 2) + resultado;
-            acarreo = suma / 2;
+            resultado = (suma % 2 == 0 ? "0" : "1") + resultado;
+            acarreo = (suma <= 1 ? 0 : 1 );
+
         }
         if (acarreo == 1) {
-            resultado = '1' + resultado;
+            resultado = "1" + resultado;
         }
-
         return resultado;
     }
-
     public static void main(String[] args) {
-        String bin1 = "100";
+        String bin1 = "111";
         String bin2 = "111";
 
         String resultado = sumarBinarios(bin1, bin2);
